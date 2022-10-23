0611184f47f7cfda3ae677e55923ed678a5293e1
Sven-D <sd_spam@outlook.de>
2022-10-23 13:25:46 +0200

    No Decorator, time in function

--- Skript.py
+++ Skript.py
@@ -12,20 +12,10 @@ import os
 from glob import glob
 from itertools import chain
 
-# This function shows the execution time of the decorated function 
-def timer_func(func):
-    def wrap_func(*args, **kwargs):
-        t1 = time()
-        result = func(*args, **kwargs)
-        t2 = time()
-        print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s')
-        return result
-    return wrap_func
 
-
-@timer_func
-def db_loader (iterator): 
-#Establish DB connection
+def db_loader (iterator, granularity_string):
+    t1 = time()
+    #Establish DB connection
     with InfluxDBClient(url="http://localhost:8086/", token=token, org=org) as client:
             #Iterate through the generator 
             for x in iterator: 
@@ -39,8 +29,13 @@ def db_loader (iterator):
                 
                 write_api = client.write_api(write_options=SYNCHRONOUS, point_settings=point_settings)
                 write_api.write(bucket="benzin", record=df, data_frame_measurement_name="benzin_test_cases")
-             
+    t2 = time() 
+    print(f' {granularity_string} : {(t2-t1):.4f}s')
 
+
+    
+
+       
             
 #Setting global parameters
 token = "80lVAVV_krqomBP3xSx-9euqajzYtAnHc1GLBUH6L1TbuOfE0a3T-a1llh0IPvplhz01M-9xGDizEa7Gwr0uNA=="
@@ -51,7 +46,7 @@ file_directory = 'C:\\Users\\Sven\\Downloads\\Benzin'
 #Crawling a dictionary and returning all .csv as a generator 
 result = (chain.from_iterable(glob(os.path.join(x[0], '*.csv')) for x in os.walk(file_directory)))
              
-db_loader(result)
+db_loader(result,"For a test")
 
 
 
